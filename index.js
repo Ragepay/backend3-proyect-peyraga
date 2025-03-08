a50cafc27bd3cf04dc61588cfdee96098ce8361f
Benjamin <benjapey99@gmial.com>
2025-03-08 12:08:52 -0300

    Listo la clase 6

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ const { PORT, MODE } = envUtil;
 
 const app = express();
 
-
+/*
 // Servidor simple.
 // Listen
 app.listen(PORT, ready);
+*/
 
-/*
 // Servidor con Clusterizacion
 if (cluster.isPrimary) {
     for (let index = 1; index <= cpus().length; index++) {
@@ -37,7 +37,7 @@ if (cluster.isPrimary) {
     // Listen
     app.listen(PORT, ready);
 }
-*/
+
 // Funcion de ejecucion del servidor.
 async function ready() {
     loggerUtil.INFO(`MODE: ${MODE} | PORT: ${PORT}`);
